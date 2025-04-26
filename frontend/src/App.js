41473e08eb2e14d94c6c653bc75b6f789092e2b1
Prachichavhan1706 <prachichavhan0606@gmail.com>
2025-04-26 19:13:13 +0530

    Added public folder and updated App.js, package.json

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
-import Fandq from "./Fandq"; // adjust the path if needed
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Login from "./Login";
+import SignUp from "./SignUp";
 
 function App() {
-  return <Fandq />;
+  return (
+    <Router>
+      <Routes>
+        
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        {/* Add a default route to handle 404 or undefined routes */}
+        <Route path="/" element={<SignUp />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
